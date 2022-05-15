25236b518b67c3f4f1b4786892234c22efcef7a0
Ray Del Rosario <hello@raydelrosario.com>
2022-05-15 18:48:52 -0500

    Update descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,16 @@
 output "bucket_name" {
-  value = aws_s3_bucket.main.id
+  description = "Name of the created S3 bucket"
+  value       = aws_s3_bucket.main.id
 }
 
 output "iam_user_access_key_id" {
-  value     = aws_iam_access_key.main.id
-  sensitive = true
+  description = "Access Key ID for access to the S3 bucket"
+  value       = aws_iam_access_key.main.id
+  sensitive   = true
 }
 
 output "iam_user_secret_access_key" {
+  description = "Secret Access Key for access to the S3 bucket"
   value     = aws_iam_access_key.main.secret
   sensitive = true
 }
--- variables.tf
+++ variables.tf
@@ -1,18 +1,22 @@
 variable "bucket_name" {
   type = string
+  description = "Name of the S3 bucket"
 }
 
 variable "iam_user_name" {
   type    = string
   default = "litestream"
+  description = "Name of the IAM user"
 }
 
 variable "iam_policy_name" {
   type    = string
   default = "litestream-access"
+  description = "Name of the IAM policy"
 }
 
 variable "iam_policy_description" {
   type    = string
   default = "The access policy for the Litestream S3 bucket"
+  description = "Description of the IAM policy"
 }
