0a7fdc68bf77f9aaaba8b99ba7a964c9607f2ea0
Ray Del Rosario <51274850+raydelrosario@users.noreply.github.com>
2023-04-14 09:12:01 -0500

    Add aws_s3_bucket_ownership_controls

--- main.tf
+++ main.tf
@@ -2,7 +2,16 @@ resource "aws_s3_bucket" "main" {
   bucket = var.bucket_name
 }
 
+resource "aws_s3_bucket_ownership_controls" "main" {
+  bucket = aws_s3_bucket.main.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
 resource "aws_s3_bucket_acl" "main" {
+  depends_on = [aws_s3_bucket_ownership_controls.main]
+
   bucket = aws_s3_bucket.main.id
   acl    = "private"
 }
